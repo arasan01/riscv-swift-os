66a8c087c53384495bc52fa39285b59508fd4140
arasan01 <developer@arasan01.com>
2024-07-23 23:13:34 +0900

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rm -f nor.S sec.S
+rm -f _swiftcode.S _swiftcode.o
 
 xcrun --toolchain org.swift.59202407221a \
   swiftc -target riscv32-none-none-eabi \
@@ -10,15 +10,16 @@ xcrun --toolchain org.swift.59202407221a \
   -Xlinker -dead_strip \
   -parse-as-library \
   -c -S \
-  a.swift > sec.S
+  a.swift > _swiftcode.S
 
 xcrun --toolchain org.swift.59202407221a \
   swiftc -target riscv32-none-none-eabi \
   -enable-experimental-feature Embedded \
   -Xcc -march=rv32gc -Xcc -mabi=ilp32d \
   -wmo \
+  -Xfrontend -function-sections \
   -O \
   -Xlinker -dead_strip \
   -parse-as-library \
-  -c -S \
-  a.swift > nor.S
+  -c -o _swiftcode.o \
+  a.swift
